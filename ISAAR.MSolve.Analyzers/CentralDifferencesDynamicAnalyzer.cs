18e08b043f9f54209284743ffe40b9b5bba4730e
VasilisMerevis <vasilis.merevis@gmail.com>
2016-10-13 15:57:50 +0300

    CentralDifferencesDynamicAnalyzer: Added InitializeInternalVectors() method.

--- ISAAR.MSolve.Analyzers/CentralDifferencesDynamicAnalyzer.cs
+++ ISAAR.MSolve.Analyzers/CentralDifferencesDynamicAnalyzer.cs
@@ -33,6 +33,36 @@ namespace ISAAR.MSolve.Analyzers
         private IAnalyzer childAnalyzer;
         private IAnalyzer parentAnalyzer;
 
+        private void InitializeInternalVectors()
+        {
+            uu.Clear();
+            uum.Clear();
+            uc.Clear();
+            ucc.Clear();
+            u.Clear();
+            v.Clear();
+            v1.Clear();
+            v2.Clear();
+            rhs.Clear();
+
+            foreach (ISolverSubdomain subdomain in subdomains.Values)
+            {
+                int dofs = subdomain.RHS.Length;
+                uu.Add(subdomain.ID, new Vector<double>(dofs));
+                uum.Add(subdomain.ID, new Vector<double>(dofs));
+                uc.Add(subdomain.ID, new Vector<double>(dofs));
+                ucc.Add(subdomain.ID, new Vector<double>(dofs));
+                u.Add(subdomain.ID, new Vector<double>(dofs));
+                v.Add(subdomain.ID, new Vector<double>(dofs));
+                v1.Add(subdomain.ID, new Vector<double>(dofs));
+                v2.Add(subdomain.ID, new Vector<double>(dofs));
+                rhs.Add(subdomain.ID, new Vector<double>(dofs));
+
+                // Account for initial conditions coming from a previous solution
+                subdomain.Solution.CopyTo(v[subdomain.ID].Data, 0);
+            }
+        }
+
         #region IAnalyzer Members
 
         public Dictionary<int, IAnalyzerLog[]> Logs { get { return null; } }
@@ -48,7 +78,24 @@ namespace ISAAR.MSolve.Analyzers
             set { childAnalyzer = value; }
         }
 
-       
+       public void Initialize()
+        {
+            if (childAnalyzer == null) throw new InvalidOperationException("Static analyzer must contain an embedded analyzer.");
+            InitializeInternalVectors();
+            //InitializeMatrices();
+            InitializeRHSs();
+            childAnalyzer.Initialize();
+        }
+
+        public void BuildMatrices()
+        {
+
+        }
+
+        public void Solve()
+        {
+
+        }
 
         #endregion
     }
