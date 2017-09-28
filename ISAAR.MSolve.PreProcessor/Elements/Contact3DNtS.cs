5cc41937a04a615bec7d129f5d1a6ffc89fd6f97
Vasilis Merevis <vasilis.merevis@outlook.com>
2017-09-29 00:52:17 +0300

    Started building Contact3DNtS contact element

---
+++ ISAAR.MSolve.PreProcessor/Elements/Contact3DNtS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ISAAR.MSolve.PreProcessor.Interfaces;
+using ISAAR.MSolve.PreProcessor.Materials;
+using ISAAR.MSolve.Matrices.Interfaces;
+using ISAAR.MSolve.Matrices;
+
+namespace ISAAR.MSolve.PreProcessor.Elements
+{
+    class Contact3DNtS : IStructuralFiniteElement
+    {
+        private static readonly DOFType[] nodalDOFTypes = new DOFType[3] { DOFType.X, DOFType.Y, DOFType.Z };
+        private static readonly DOFType[][] dofs = new DOFType[][] { nodalDOFTypes, nodalDOFTypes, nodalDOFTypes, nodalDOFTypes };
+        private readonly IFiniteElementMaterial material;
+        private IFiniteElementDOFEnumerator dofEnumerator = new GenericDOFEnumerator();
+
+        public double Density { get; set; }
+
+        private double[] node1XYInitial, node2XYInitial, node1XYCurrent, node2XYCurrent;
+        private double[] node1GlobalDisplacementVector, node2GlobalDisplacementVector;
+
+        private bool isInitializedK = false;
+        private bool isInitializedF = false;
+
+        public Contact3DNtS(IFiniteElementMaterial3D material)
+        {
+            
+        }
+
+        public Contact3DNtS(IFiniteElementMaterial3D material, IFiniteElementDOFEnumerator dofEnumerator)
+            : this(material)
+        {
+            this.dofEnumerator = dofEnumerator;
+        }
+    }
+}
