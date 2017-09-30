5514355868b9316f8c87bc5a364b2462db0e5421
Vasilis Merevis <vasilis.merevis@outlook.com>
2017-09-30 20:27:22 +0300

    Contact3DNtS class - Implemented CalculateShapeFunctions method

--- ISAAR.MSolve.PreProcessor/Elements/Contact3DNtS.cs
+++ ISAAR.MSolve.PreProcessor/Elements/Contact3DNtS.cs
@@ -34,5 +34,41 @@ namespace ISAAR.MSolve.PreProcessor.Elements
         {
             this.dofEnumerator = dofEnumerator;
         }
+
+        private Tuple<Dictionary<int, double>, Dictionary<int, double>> CalculateShapeFunctions(double ksi1, double ksi2)
+        {
+            double N1 = 1 / 4 * (1 - ksi1) * (1 - ksi2);
+            double N2 = 1 / 4 * (1 + ksi1) * (1 - ksi2);
+            double N3 = 1 / 4 * (1 + ksi1) * (1 + ksi2);
+            double N4 = 1 / 4 * (1 - ksi1) * (1 + ksi2);
+
+            Dictionary<int, double> N = new Dictionary<int, double>();
+            N[1] = N1;
+            N[2] = N2;
+            N[3] = N3;
+            N[4] = N4;
+
+            double dN11 = -1 / 4 * (1 - ksi2);
+            double dN12 = -1 / 4 * (1 - ksi1);
+            double dN21 = 1 / 4 * (1 - ksi2);
+            double dN22 = -1 / 4 * (1 + ksi1);
+            double dN31 = 1 / 4 * (1 + ksi2);
+            double dN32 = 1 / 4 * (1 + ksi1);
+            double dN41 = -1 / 4 * (1 + ksi2);
+            double dN42 = 1 / 4 * (1 - ksi1);
+
+            Dictionary<int, double> dN = new Dictionary<int, double>();
+            dN[11] = dN11;
+            dN[12] = dN12;
+            dN[21] = dN21;
+            dN[22] = dN22;
+            dN[31] = dN31;
+            dN[32] = dN32;
+            dN[41] = dN41;
+            dN[42] = dN42;
+
+            Tuple<Dictionary<int, double>, Dictionary<int, double>> shapeFunctions = new Tuple<Dictionary<int, double>, Dictionary<int, double>>(N, dN);
+            return shapeFunctions;
+        }
     }
 }
