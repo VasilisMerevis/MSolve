3a45256e2b58d10a596846bcf9c5cfa7566cb6be
Vasilis Merevis <vasilis.merevis@outlook.com>
2018-11-20 23:44:31 +0200

    Addition of new Quads example. Addition of Thickness.

--- ISAAR.MSolve.SamplesConsole/TwoQuadsExample.cs
+++ ISAAR.MSolve.SamplesConsole/TwoQuadsExample.cs
@@ -17,30 +17,24 @@ using ISAAR.MSolve.Materials;
 
 namespace ISAAR.MSolve.SamplesConsole
 {
-    public class ContactNtNTwoBLocks
+    public class TwoQuadsExample
     {
         public static IList<Node> CreateNodes()
         {
             IList<Node> nodes = new List<Node>();
             Node node1 = new Node { ID = 1, X = 0, Y = 0 };
-            Node node2 = new Node { ID = 2, X = 1.0, Y = 0 };
-            Node node3 = new Node { ID = 3, X = 1.0, Y = 1.0 };
-            Node node4 = new Node { ID = 4, X = 0, Y = 1.0 };
-
-            Node node5 = new Node { ID = 5, X = 0, Y = 1.01 };
-            Node node6 = new Node { ID = 6, X = 1.0, Y = 1.01 };
-            Node node7 = new Node { ID = 7, X = 1.0, Y = 2.01 };
-            Node node8 = new Node { ID = 8, X = 0, Y = 2.01 };
+            Node node2 = new Node { ID = 2, X = 2.0, Y = 0 };
+            Node node3 = new Node { ID = 3, X = 2.0, Y = 2.0 };
+            Node node4 = new Node { ID = 4, X = 2.0, Y = 4.0 };
+            Node node5 = new Node { ID = 5, X = 0.0, Y = 4.0 };
+            Node node6 = new Node { ID = 6, X = 0.0, Y = 2.0 };
 
             nodes.Add(node1);
             nodes.Add(node2);
             nodes.Add(node3);
             nodes.Add(node4);
-
             nodes.Add(node5);
             nodes.Add(node6);
-            nodes.Add(node7);
-            nodes.Add(node8);
 
             return nodes;
         }
@@ -48,12 +42,12 @@ namespace ISAAR.MSolve.SamplesConsole
         public static void Run()
         {
             VectorExtensions.AssignTotalAffinityCount();
-            double youngMod = 100000.0;
+            double youngMod = 200.0e9;
             double poisson = 0.3;
-            double loadY = 50000.0;
-            
+            double load = 1.0e9;
 
-            IList<Node> nodes = ContactNtNTwoBLocks.CreateNodes();
+
+            IList<Node> nodes = TwoQuadsExample.CreateNodes();
 
             Model twoBlocks = new Model();
 
@@ -68,12 +62,6 @@ namespace ISAAR.MSolve.SamplesConsole
             twoBlocks.NodesDictionary[1].Constraints.Add(DOFType.Y);
             twoBlocks.NodesDictionary[2].Constraints.Add(DOFType.X);
             twoBlocks.NodesDictionary[2].Constraints.Add(DOFType.Y);
-            twoBlocks.NodesDictionary[3].Constraints.Add(DOFType.X);
-            twoBlocks.NodesDictionary[4].Constraints.Add(DOFType.X);
-            twoBlocks.NodesDictionary[5].Constraints.Add(DOFType.X);
-            twoBlocks.NodesDictionary[6].Constraints.Add(DOFType.X);
-            twoBlocks.NodesDictionary[7].Constraints.Add(DOFType.X);
-            twoBlocks.NodesDictionary[8].Constraints.Add(DOFType.X);
 
             ElasticMaterial2D material = new ElasticMaterial2D(StressState2D.PlaneStress)
             {
@@ -82,40 +70,28 @@ namespace ISAAR.MSolve.SamplesConsole
             };
 
 
-            var element1 = new Element() { ID = 1, ElementType = new Quad4(material) };
-            var element2 = new Element() { ID = 2, ElementType = new Quad4(material) };
-            
-            var element3 = new Element() { ID = 3, ElementType = new Contact2DNtN(youngMod) };
-            var element4 = new Element() { ID = 4, ElementType = new Contact2DNtN(youngMod) };
+            var element1 = new Element() { ID = 1, ElementType = new Quad4(material) { Thickness = 1.0 } };
+            var element2 = new Element() { ID = 2, ElementType = new Quad4(material) { Thickness = 1.0 } };
 
             element1.AddNode(twoBlocks.NodesDictionary[1]);
             element1.AddNode(twoBlocks.NodesDictionary[2]);
             element1.AddNode(twoBlocks.NodesDictionary[3]);
-            element1.AddNode(twoBlocks.NodesDictionary[4]);
+            element1.AddNode(twoBlocks.NodesDictionary[6]);
 
-            element2.AddNode(twoBlocks.NodesDictionary[5]);
             element2.AddNode(twoBlocks.NodesDictionary[6]);
-            element2.AddNode(twoBlocks.NodesDictionary[7]);
-            element2.AddNode(twoBlocks.NodesDictionary[8]);
-
-            element3.AddNode(twoBlocks.NodesDictionary[4]);
-            element3.AddNode(twoBlocks.NodesDictionary[5]);
-
-            element4.AddNode(twoBlocks.NodesDictionary[3]);
-            element4.AddNode(twoBlocks.NodesDictionary[6]);
+            element2.AddNode(twoBlocks.NodesDictionary[3]);
+            element2.AddNode(twoBlocks.NodesDictionary[4]);
+            element2.AddNode(twoBlocks.NodesDictionary[5]);
 
             twoBlocks.ElementsDictionary.Add(element1.ID, element1);
             twoBlocks.ElementsDictionary.Add(element2.ID, element2);
-            twoBlocks.ElementsDictionary.Add(element3.ID, element3);
-            twoBlocks.ElementsDictionary.Add(element4.ID, element4);
 
             twoBlocks.SubdomainsDictionary[0].ElementsDictionary.Add(element1.ID, element1);
             twoBlocks.SubdomainsDictionary[0].ElementsDictionary.Add(element2.ID, element2);
-            twoBlocks.SubdomainsDictionary[0].ElementsDictionary.Add(element3.ID, element3);
-            twoBlocks.SubdomainsDictionary[0].ElementsDictionary.Add(element4.ID, element4);
 
-            twoBlocks.Loads.Add(new Load() { Amount = loadY, Node = twoBlocks.NodesDictionary[7], DOF = DOFType.Y });
-            twoBlocks.Loads.Add(new Load() { Amount = loadY, Node = twoBlocks.NodesDictionary[8], DOF = DOFType.Y });
+            twoBlocks.Loads.Add(new Load() { Amount = load, Node = twoBlocks.NodesDictionary[4], DOF = DOFType.X });
+            twoBlocks.Loads.Add(new Load() { Amount = -load, Node = twoBlocks.NodesDictionary[4], DOF = DOFType.Y });
+            twoBlocks.Loads.Add(new Load() { Amount = -load, Node = twoBlocks.NodesDictionary[5], DOF = DOFType.Y });
 
             twoBlocks.ConnectDataStructures();
 
@@ -130,11 +106,22 @@ namespace ISAAR.MSolve.SamplesConsole
             var linearSystemsArray = new[] { linearSystems[0] };
             NewtonRaphsonNonLinearAnalyzer childAnalyzer = new NewtonRaphsonNonLinearAnalyzer(solver, linearSystemsArray, subdomainUpdaters,
                 subdomainMappers, provider, 10, totalDOFs);
+
+            //var linearSystems = new Dictionary<int, ILinearSystem>(); //I think this should be done automatically
+            //linearSystems[0] = new SkylineLinearSystem(0, twoBlocks.SubdomainsDictionary[0].Forces);
+            //SolverSkyline solver = new SolverSkyline(linearSystems[0]);
+
+            //ProblemStructural provider = new ProblemStructural(twoBlocks, linearSystems);
+
+            //LinearAnalyzer childAnalyzer = new LinearAnalyzer(solver, linearSystems);
+            //StaticAnalyzer parentAnalyzer = new StaticAnalyzer(provider, childAnalyzer, linearSystems);
+
+
             StaticAnalyzer parentAnalyzer = new StaticAnalyzer(provider, childAnalyzer, linearSystems);
 
             childAnalyzer.LogFactories[0] = new LinearAnalyzerLogFactory(new int[] {
-                twoBlocks.NodalDOFsDictionary[3][DOFType.Y],
-                twoBlocks.NodalDOFsDictionary[4][DOFType.Y]
+                twoBlocks.NodalDOFsDictionary[4][DOFType.Y],
+                twoBlocks.NodalDOFsDictionary[5][DOFType.Y]
                 });
 
             parentAnalyzer.BuildMatrices();
@@ -146,3 +133,4 @@ namespace ISAAR.MSolve.SamplesConsole
         }
     }
 }
+
